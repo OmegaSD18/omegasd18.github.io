c859e89890715221e5a632c782a6004b763eb720
Omegasd18 <soumya18vk@gmail.com>
2025-05-28 23:26:03 +0530

    menu trigger fixed

--- assets/js/extended/menu-trigger.js
+++ assets/js/extended/menu-trigger.js
@@ -1,16 +1,12 @@
 let menuTrigger=document.querySelector("#menu-trigger");
 let menu=document.querySelector(".menu");
-
-menuTrigger.addEventListener("click",function(){ 
-    menu.classList.toggle("hidden")
-});
-
-document.body.addEventListener('click', function(a) {
-    menuTrigger.contains(a.target) || menu.classList.add("hidden");
-});
-
 const dropdownButton = document.querySelector('.dropdown-button');
 
 dropdownButton.addEventListener('click', () => {
     dropdownButton.classList.toggle('active');
+    menuTrigger.contains(a.target) || menu.classList.add("hidden");
+});
+
+menuTrigger.addEventListener("click",function(){ 
+    menu.classList.toggle("hidden")
 });
